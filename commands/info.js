023035bbfec0b5233bea9ef81b4f34040b9018f5
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-06 00:53:28 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
     .setTitle("Info")
     .setColor("#6dad3e")
     .addFields(
-        { name: "Dit is de bot van Gelderland V3 (beta)", value: "De bot is gemaakt door @Daniël#2939 en de owner van Alkmaar zijn @christian#2266, @Joey.rmu#9947 en @kansloss0100#3437"},
+        { name: "Dit is de bot van Gelderland V3 (beta)", value: "De bot is gemaakt door @Daniël#2939 en de owners van Alkmaar zijn: \n @christian#2266 \n @Joey.rmu#9947 \n @kansloss0100#3437"},
         { name: "Dit is De officiële support server van de bot: Daniël bot", value: "[Klik Hier!](https://discord.gg/2E5fhn3)" },
     )
     .setThumbnail("https://media.discordapp.net/attachments/727563359276236950/728606802152914984/image0.png?width=400&height=300")
